632243b9e80fb7fd3789ee827bea2955524c2b27
Zi Onn <zionn@zionn.local>
2023-12-15 13:56:34 +0800

    add python set example

---
+++ code/examples/sets.py
@@ -0,0 +1,9 @@
+# this will create a empty dictionary
+numbers = {}
+print(type(numbers))
+print(numbers)
+
+# while this will create a set
+numbers = {1, 2, 3, 4, 5}
+print(type(numbers))
+print(numbers)
